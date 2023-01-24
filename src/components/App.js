e83dd0f82bcbe87e0eb1e1dd2e67b448f3576c03
trevor dewindt <trevor.dewindt@gmail.com>
2023-01-23 19:01:44 -0700

    clean up console errors and test api routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
+import {useState, useEffect} from 'react'
+
 function App() {
+  const {clients, setClients} = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:9292/clients')
+    .then((res) => res.json())
+    .then(data => console.log(data))
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
         <p>
           Training Catalog This is a test
         </p>
       </header>
+        <p>test</p>
     </div>
   );
 }
