7132db7f3258083c68bfea643427c98242db3703
trevor dewindt <trevor.dewindt@gmail.com>
2023-01-23 18:39:38 -0700

    clean up frontend repo

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Training Catalog
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
