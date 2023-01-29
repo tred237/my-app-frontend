980a5a457eaf249e15c3344097cf955d8862d48b
trevor dewindt <trevor.dewindt@gmail.com>
2023-01-28 20:02:07 -0700

    create go home button on routines pages

--- src/components/RoutineList.js
+++ src/components/RoutineList.js
@@ -1,11 +1,12 @@
+import React from 'react'
 import { useState, useEffect } from 'react'
-import { useParams } from "react-router-dom"
+import { useParams, useHistory } from "react-router-dom"
 import Routine from './Routine'
 
 function RoutineList({ clients, onRoutinesChange }){
     const [routines, setRoutines] = useState([])
     const { clientId } = useParams()
-
+    const history = useHistory()
 
     useEffect(() => {
         const newRoutines = (clients.filter(e => e.id == clientId)[0].routines)
@@ -13,9 +14,12 @@ function RoutineList({ clients, onRoutinesChange }){
     },[clientId])
 
     return(
-        <ul>
-            {routines.map(e => <Routine key={e.id} routine={e}/>)}
-        </ul>
+        <React.Fragment>
+            <button onClick={() => history.push("/")}>Go Home</button>
+            <ul>
+                {routines.map(e => <Routine key={e.id} routine={e}/>)}
+            </ul>
+        </React.Fragment>
     )
 }
 
