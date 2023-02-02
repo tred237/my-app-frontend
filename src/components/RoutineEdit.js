d1ce48ec63d4721d4ea1ef051e07c91436cedc5f
trevor dewindt <trevor.dewindt@gmail.com>
2023-02-01 21:49:50 -0700

    create routine edit form component and move form to it

--- src/components/RoutineEdit.js
+++ src/components/RoutineEdit.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import RoutineEditForm from './RoutineEditForm'
 
 function RoutineEdit({ routine, clientId, onSetEdit, onRoutineDelete, onRoutineUpdate }) {
     const [routineData, setRoutineData] = useState({
@@ -58,30 +59,7 @@ function RoutineEdit({ routine, clientId, onSetEdit, onRoutineDelete, onRoutineU
     
     return(
         <React.Fragment>
-            <form onSubmit={handleRoutineEditSubmit}>
-                <label>Day of Week: 
-                    <input type="text" name="day" value={routineData.day} onChange={handleRoutineChange} required/>
-                </label>
-                <label>Exercise: 
-                    <input type="text" name="exercise" value={routineData.exercise} onChange={handleRoutineChange} required/>
-                </label>
-                <label>Exercise Type: 
-                    <input type="text" name="exercise_type" value={routineData.exercise_type} onChange={handleRoutineChange} required/>
-                </label>
-                <label>Sets: 
-                    <input type="text" name="sets" value={routineData.sets} onChange={handleRoutineChange}/>
-                </label>
-                <label>Reps: 
-                    <input type="text" name="reps" value={routineData.reps} onChange={handleRoutineChange}/>
-                </label>
-                <label>Distance (Miles): 
-                    <input type="text" name="distance_miles" value={routineData.distance_miles} onChange={handleRoutineChange}/>
-                </label>
-                <label>Length of Time (Minutes): 
-                    <input type="text" name="length_of_time_minutes" value={routineData.length_of_time_minutes} onChange={handleRoutineChange}/>
-                </label>
-                <button type="submit">Save</button>
-            </form>
+            <RoutineEditForm routineData={routineData} onRoutineEditSubmit={handleRoutineEditSubmit} onRoutineChange={handleRoutineChange} />
             <button onClick={onSetEdit}>Cancel</button>
             <button onClick={handleRoutineDelete}>Delete</button>
         </React.Fragment>
---
+++ src/components/RoutineEditForm.js
@@ -0,0 +1,30 @@
+function RoutineEditForm({ routineData, onRoutineEditSubmit, onRoutineChange }){
+    return(
+        <form onSubmit={onRoutineEditSubmit}>
+            <label>Day of Week: 
+                <input type="text" name="day" value={routineData.day} onChange={onRoutineChange} required/>
+            </label>
+            <label>Exercise: 
+                <input type="text" name="exercise" value={routineData.exercise} onChange={onRoutineChange} required/>
+            </label>
+            <label>Exercise Type: 
+                <input type="text" name="exercise_type" value={routineData.exercise_type} onChange={onRoutineChange} required/>
+            </label>
+            <label>Sets: 
+                <input type="text" name="sets" value={routineData.sets} onChange={onRoutineChange}/>
+            </label>
+            <label>Reps: 
+                <input type="text" name="reps" value={routineData.reps} onChange={onRoutineChange}/>
+            </label>
+            <label>Distance (Miles): 
+                <input type="text" name="distance_miles" value={routineData.distance_miles} onChange={onRoutineChange}/>
+            </label>
+            <label>Length of Time (Minutes): 
+                <input type="text" name="length_of_time_minutes" value={routineData.length_of_time_minutes} onChange={onRoutineChange}/>
+            </label>
+            <button type="submit">Save</button>
+        </form>
+    )
+}
+
+export default RoutineEditForm
